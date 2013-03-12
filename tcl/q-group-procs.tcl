2a1c2a301120d59095fd996014ab16e89f0d09d8
Benjamin Brink <kappa@dekka.com>
2013-03-12 06:19:29 +0000

    pre-tested contribution procs

--- tcl/q-group-procs.tcl
+++ tcl/q-group-procs.tcl
@@ -20,8 +20,17 @@ ad_proc -public qw_contributors {
         # set instance_id package_id
         set instance_id [ad_conn package_id]
     }
-    
-
+    if { $template_id ne "" } {
+        set rev_user_ids_list [db_list_of_lists qw_pg_contributor_user_ids { select user_id from qw_wiki_page where template_id = :template_id and instance_id =:instance_id order by last_modified desc } ]
+    } else {
+        set rev_user_ids_list [db_list_of_lists qw_contributor_user_ids { select user_id from qw_wiki_page where instance_id =:instance_id order by last_modified desc } ]
+    }
+    set user_id_list [list ]
+    foreach user_id $rev_user_ids_list {
+        if { [lsearch -exact $user_id_list $user_id] == -1 } {
+            lappend user_id_list $user_id
+        }
+    }
     return $user_id_list
 }
 
@@ -29,15 +38,23 @@ ad_proc -public qw_most_recent_edit_stats {
     {template_id ""}
     {instance_id ""}
 } {
-    Returns user_id and last_modified for most recent edit of template_id or instance_id
+    Returns user_id, last_modified and page_id (revision) as list for most recent edit of template_id or instance_id
 } {
     if { $instance_id eq "" } {
         # set instance_id package_id
         set instance_id [ad_conn package_id]
     }
-    
-    set page_exists_p [db_0or1row wiki_page_get_id {select name from qw_wiki_page where id = :page_id and instance_id = :instance_id } ]
-    return $page_exists_p
+    if { $template_id ne "" } {
+        rev_exists_p [db_0or1row qw_most_recent_template_edits { select user_id, last_modified from qw_wiki_page where template_id = :template_id and instance_id = :instance_id order by last_modified desc limit 1 } ]
+    } else {
+        rev_exists_p [db_0or1row qw_most_recent_instance_edits { select user_id, last_modified from qw_wiki_page where template_id = :template_id and instance_id = :instance_id order by last_modified desc limit 1 } ]
+    }
+    if { $rev_exists_p } {
+        set stats_list [list $user_id $last_modified $page_id]
+    } else {
+        set stats_list [list ]
+    }
+    return $stats_list
 }
 
 ad_proc -public qw_user_contributions {
@@ -50,10 +67,10 @@ ad_proc -public qw_user_contributions {
         # set instance_id package_id
         set instance_id [ad_conn package_id]
     }
-    
-    set page_exists_p [db_0or1row wiki_page_get_id {select name from qw_wiki_page where id = :page_id and instance_id = :instance_id } ]
-    return $page_exists_p
+    if { $user_id eq "" } {
+        set user_id [ad_conn user_id]
+    }
+    set page_ids_list [db_list_of_lists qw_contributor_page_ids { select page_id from qw_wiki_page where instance_id =:instance_id order by last_modified desc } ]    
 
-
-
+    return $page_ids_list
 }
