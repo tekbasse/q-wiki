db93bb3e9bccc85fcf0955ec291c26687df89afa
Benjamin Brink <tekbasse@yahoo.com>
2015-05-16 04:23:07 -0400

    added template set proc

--- tcl/q-wiki-procs.tcl
+++ tcl/q-wiki-procs.tcl
@@ -42,6 +42,70 @@ ad_proc -public qw_template_custom_read {
     return $custom_fields
 }
 
+ad_proc -public qw_template_custom_set {
+    report_ref
+    default_fields
+    custom_fields
+    {instance_id ""}
+    {user_id ""}
+} {
+    This procedure sets a custom order for use with templates, where a template contains ordered references, 
+    $1 .. $9. By supplying a custom order of fields for a report, admins (and possibly users) can control the
+    order of fields displayed in a table or responsive list substitute for an html TABLE.
+     Limit length of space separated list: 320 characters.
+} {
+    if { $instance_id eq "" } {
+        # set instance_id package_id
+        set instance_id [ad_conn package_id]
+    }
+    set success_p 1
+    # get current custom (if any) to see if a write is necessary
+    set custom_fields_old [qw_template_custom_read $report_ref $default_fields $instance_id $user_id]
+    if { $custom_fields_old ne $custom_fields } {
+        # update or create it
+        set report_ref [string range $report_ref 0 79]
+        if { $user_id eq "" } {
+            # set for all cases
+            db_0or1row qw_template_custom_map_all_ck "select custom_order as custom_order_old2 from qw_template_custom_map where instance_id =:instance_id and report_ref=:report_ref and default_fields =:default_fields and user_id is null"
+            if { [info exists custom_order_old2] } {
+                # record exists. 
+                if { $custom_order_old2 ne $custom_fields } {
+                    # update record
+                    db_dml { update qw_template_custom_map 
+                        set custom_order =:custom_fields where instance_id=:instance_id and report_ref=:report_ref and default_fields=:default_fields and  user_id is null
+                    }
+                }
+            } else {
+                # create record
+                db_dml { insert into qw_template_custom_map 
+                    (custom_fields,instance_id,report_ref,default_fields)
+                    values (:custom_order,:instance_id,:report_ref,:default_fields)
+                }
+            }
+        } elseif { [qf_is_natural_number $user_id] } {
+            # set for this user
+            db_0or1row qw_template_custom_map_all_ck "select custom_order as custom_order_old2 from qw_template_custom_map where instance_id =:instance_id and report_ref=:report_ref and default_fields =:default_fields and user_id =:user_id"
+            if { [info exists custom_order_old2] } {
+                # record exists. 
+                if { $custom_order_old2 ne $custom_fields } {
+                    # update record
+                    db_dml { update qw_template_custom_map 
+                        set custom_order =:custom_fields where instance_id=:instance_id and report_ref=:report_ref and default_fields=:default_fields and  user_id =:user_id
+                    }
+                }
+            } else {
+                # create record
+                db_dml { insert into qw_template_custom_map 
+                    (custom_fields,instance_id,report_ref,default_fields,:user_id)
+                    values (:custom_order,:instance_id,:report_ref,:default_fields,:user_id)
+                }
+            }
+        } else {
+            set success_p 0
+        }
+    }
+    return $success_p 
+}
 
 ad_proc -public qw_page_id_exists { 
     page_id
